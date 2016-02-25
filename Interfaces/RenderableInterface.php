6c626740daf640d8b2ef840e7119dd7c54a9aa8d
Cláudio Silva <claudio.silva@impactwave.com>
2016-02-25 23:55:00 +0000

    -

--- Interfaces/RenderableInterface.php
+++ Interfaces/RenderableInterface.php
@@ -9,7 +9,7 @@ namespace Selenia\Interfaces;
 interface RenderableInterface
 {
   /**
-   * Gets the class name of the context that the component requires to be set for standalone rendering.
+   * Gets the class name of the context object that the component requires in order to be able to render itself.
    *
    * @return string
    */
@@ -26,7 +26,7 @@ interface RenderableInterface
    * Sets the component's rendering context.
    *
    * <p>This can be called by a router when the component is rendered directly from a route, or by a parent component
-   * on a DOM.
+   * on a document.
    *
    * > <p>Matisse components require an argument of type {@see Context}.
    *
