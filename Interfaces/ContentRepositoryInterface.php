475fc1a122a2914e771689e54d0facf1258ee884
Cláudio Silva <claudio.silva@impactwave.com>
2016-03-03 18:03:22 +0000

    ContentRepositoryInterface

---
+++ Interfaces/ContentRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace Selenia\Interfaces;
+
+interface ContentRepositoryInterface
+{
+  /**
+   * Get the URL of an image in the content repository, with optional processing settings.
+   *
+   * <p>If `$path` is `null` or `''`, `''` is returned.
+   *
+   * @param  string $path   The virtual resource path.
+   * @param  array  $params A map of manipulation parameters.
+   * @return string The URL to retrieve the image.
+   */
+  public function getImageUrl ($path, array $params = []);
+
+}
