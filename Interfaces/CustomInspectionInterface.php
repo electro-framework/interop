bd463e696db7f12a8762b748995549e4fc204984
Cláudio Silva <claudio.silva@impactwave.com>
2016-03-21 23:01:04 +0000

    CustomInspectionInterface

---
+++ Interfaces/CustomInspectionInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace Selenia\Interfaces;
+
+/**
+ * Indicates that a class provides custom logic for rending its inspection (debugging) representation.
+ */
+interface CustomInspectionInterface
+{
+  /**
+   * Renders the instance's state and returns the resulting markup.
+   *
+   * @return string
+   */
+  function inspect ();
+
+}
