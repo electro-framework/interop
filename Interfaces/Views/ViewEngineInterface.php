c45c0b8f0e2d1680005e80cc17478a875d16a722
Cláudio Silva <claudio.silva@impactwave.com>
2016-04-19 19:41:20 +0100

    Doc update.

--- Interfaces/Views/ViewEngineInterface.php
+++ Interfaces/Views/ViewEngineInterface.php
@@ -24,9 +24,13 @@ interface ViewEngineInterface
   function configure ($options);
 
   /**
+   * Renders the compiled template.
+   *
    * @param mixed        $template The compiled template,or the original source code if the engine does not support
    *                               template compilation. When called from a View, this is guaranteed to be non-null.
-   * @param array|object $data     The view model; optional data for use by databinding expressions on the template.
+   * @param array|object $data     The view model; optional data for use by data binding expressions on the template.
+   *                               <p>Note: Matisse components ignore this parameter, as the view model is set by other
+   *                               means.
    * @return string The generated output (ex: HTML).
    */
   function render ($template, $data = null);
