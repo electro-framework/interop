8908c81fbf2cd9df022f4cdf2934845cba546471
Cláudio Silva <claudio.silva@impactwave.com>
2016-04-29 00:24:51 +0100

    Change FlashType constants.

--- Exceptions/FlashType.php
+++ Exceptions/FlashType.php
@@ -3,8 +3,9 @@ namespace Selenia\Exceptions;
 
 class FlashType
 {
-  const FATAL   = 0;
+  const NEUTRAL = 0;
   const ERROR   = 1;
   const WARNING = 2;
   const INFO    = 3;
+  const SUCCESS = 4;
 }
