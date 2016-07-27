c9ac07d6b4eafdb24c0b34552074a7c3f8065f35
Cláudio Silva <claudio.silva@impactwave.com>
2016-07-27 19:34:55 +0100

    New way to assemble the middleware: the ApplicationMiddlwareAssemblerInterface.

---
+++ Interfaces/Http/ApplicationMiddlewareAssemblerInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace Electro\Interfaces\Http;
+
+/**
+ * A class responsible for assembling the application's main middleware pipeline.
+ *
+ * This interface allows the application to override the framework's predefined middleware and assemble a completely
+ * customized alternative.
+ */
+interface ApplicationMiddlewareAssemblerInterface
+{
+  /**
+   * Assembles the middleware pipeline.
+   *
+   * @param MiddlewareStackInterface $stack The stack to which middleware should be added.
+   */
+  function assemble (MiddlewareStackInterface $stack);
+}
--- Interfaces/Http/Shared/ApplicationMiddlewareInterface.php
+++ Interfaces/Http/Shared/ApplicationMiddlewareInterface.php
@@ -6,17 +6,16 @@ use Electro\Interfaces\Http\MiddlewareStackInterface;
 /**
  * Represents the application's main/root HTTP middleware stack.
  *
- * <p>Application modules can add their middlewares to this stack by injecting the shared instance of this interface
+ * <p>Application modules can add their middleware to this stack by injecting the shared instance of this interface
  * and using its API.
  *
  * ###### Example:
  * > ```
  * class MyModule
  * {
- *   function configure (RootMiddlewareStackInterface $stack)
+ *   function configure (ApplicationMiddlewareInterface $stack)
  *   {
- *     $myMiddleware = ...
- *     $stack->add ($myMiddleware);
+ *     $stack->add ([...]);
  *   }
  * }
  * > ```
