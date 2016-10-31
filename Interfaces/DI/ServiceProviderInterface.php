25fe46c96e0960e047ece8e817fdfcea146d1318
Cláudio Silva <claudio.silva@impactwave.com>
2016-10-31 01:22:54 +0000

    ObserverTrait and minor doc updates.

--- Interfaces/DI/ServiceProviderInterface.php
+++ Interfaces/DI/ServiceProviderInterface.php
@@ -8,7 +8,7 @@ interface ServiceProviderInterface
 {
   /**
    * Registers services on the provided dependency injector.
-   * > **Best practice:** do not use the injector to fetch dependencies here.
+   * > **Best practice:** do not use the injector to fetch dependencies here; use it only to register services.
    * @param InjectorInterface $injector
    */
   function register (InjectorInterface $injector);
--- Interfaces/ModuleInterface.php
+++ Interfaces/ModuleInterface.php
@@ -10,24 +10,35 @@ namespace Electro\Interfaces;
  *
  * You may implement the following methods:
  *
+ * <hr>
+ *
  * ### void public function boot(...)
  *
  * Allows a module to request services and perform actions upon them, prior to the HTTP request being sento to the
  * HTTP processing pipeline.
- * <p>This is called after all service providers are configured and have already registered their services
+ * <p>This is called after all service providers are configured (see `configure()` below) and have already registered
+ * their services (see {@see ServiceProviderInterface}). Furthermore, the {@see WebServer} instance is already initialized
+ * and the modukes can access the current HTTP request; for instance, a module may conditionally register services
+ *
  * > #### Injecting services
- * This is an injectable method. You can use the injected services to setup additional
- * functionality that is provided by this provider.
+ * > This is an injectable method. You can use the injected services to setup additional functionality that is provided
+ * > by this provider.
+ *
+ * <hr>
  *
  * ### void public function configure (ModuleServices $module, ...);
  *
- * Allows a module to configure the standard module capabilities it provides.
- * <p>This is called after all service providers have registered their services.
+ * Allows a module to configure the module capabilities it provides.
+ * <p>This is called after all service providers have registered their services (see {@see ServiceProviderInterface}).
+ *
  * > #### Injecting services
- * This is an injectable method. You should almost always inject a ModuleServices instance; inject other services
- * on a needed basis.
+ * > This is an injectable method. You should almost always inject a ModuleServices instance; inject other services on
+ * > a needed basis.
  */
 interface ModuleInterface
 {
-
+  /**
+   * @return callable function (mixed ...$dependency)
+   */
+  function boot ();
 }
