cedd8e0ee71092afe813a5669b85f1d3edb7368f
Cláudio Silva <claudio.silva@impactwave.com>
2017-01-14 00:15:19 +0000

    More debugging info for HttpExceptions.

--- Exceptions/ExceptionWithTitle.php
+++ Exceptions/ExceptionWithTitle.php
@@ -14,10 +14,11 @@ class ExceptionWithTitle extends \Exception
    * @param string $title
    * @param string $msg
    * @param int    $code
+   * @param \Exception|null $previous The exception that caused this exception.
    */
-  function __construct ($title = '', $msg = '', $code = 0)
+  function __construct ($title = '', $msg = '', $code = 0, $previous = null)
   {
-    parent::__construct ($msg, $code);
+    parent::__construct ($msg, $code, $previous);
     $this->title = $title;
   }
 
--- Exceptions/HttpException.php
+++ Exceptions/HttpException.php
@@ -1,18 +1,17 @@
 <?php
 namespace Electro\Exceptions;
 
-use Electro\Exceptions;
-
 class HttpException extends ExceptionWithTitle
 {
   /**
-   * @param int    $statusCode
-   * @param string $title Error description. It should be a single line of unformatted text.
-   * @param string $msg   Additional information about the error. It may contain HTML formatting.
+   * @param int             $statusCode
+   * @param string          $title Error description. It should be a single line of unformatted text.
+   * @param string          $msg   Additional information about the error. It may contain HTML formatting.
+   * @param \Exception|null $previous The exception that caused this exception.
    */
-  function __construct ($statusCode = 500, $title = '', $msg = '')
+  function __construct ($statusCode = 500, $title = '', $msg = '', $previous = null)
   {
-    parent::__construct ($title, $msg, $statusCode);
+    parent::__construct ($title, $msg, $statusCode, $previous);
   }
 
 }
