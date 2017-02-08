04e581900990762f64ae9fdf5a3e7f3adb01a79f
Cláudio Silva <claudio.silva@impactwave.com>
2017-02-08 13:22:55 +0000

    Make CurrentRequest be a proxy for the request instance.

--- Interfaces/Http/Shared/CurrentRequestInterface.php
+++ Interfaces/Http/Shared/CurrentRequestInterface.php
@@ -2,22 +2,29 @@
 
 namespace Electro\Interfaces\Http\Shared;
 
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * A service that allows access to the ServerRequestInterface instance at the current stage of middleware/routing.
  *
- * <p>When injecting this instance, instead of saving it in a class property as usual, you should instead save the
- * request instance obtained via `get()` to capture its current value, as it may change at a later time.
+ * <p>This class is {@see ServerRequestInterface} compatible and wraps a replaceable instance of it.
+ * <p>You can use this wherever you would use a `ServerRequestInterface` instance; accessing its methods will invoke
+ * the inner instance's methods.
+ *
+ * <p>You can call `get()` to get the current Request instance, but usually do not have to; you may use an instance
+ * of this class as if it was the request itself.
+ *
+ * <p>Routers and middleware runners can set the current request by calling `set()`.
  */
-interface CurrentRequestInterface
+interface CurrentRequestInterface extends RequestInterface
 {
   /**
    * Gets the current request instance.
    *
    * @return ServerRequestInterface|null NULL if there is no request instance yet.
    */
-  function get ();
+  function getInstance ();
 
   /**
    * Sets the current request instance.
@@ -25,5 +32,5 @@ interface CurrentRequestInterface
    * @param ServerRequestInterface $req
    * @return void
    */
-  function set (ServerRequestInterface $req);
+  function setInstance (ServerRequestInterface $req);
 }
