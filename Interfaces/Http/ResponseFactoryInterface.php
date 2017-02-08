87d5c9284b0279129f4e52fe64734035aef76d84
Cláudio Silva <claudio.silva@impactwave.com>
2017-02-08 11:50:27 +0000

    Changes to RequestFactoryInterface.

--- Interfaces/Http/ResponseFactoryInterface.php
+++ Interfaces/Http/ResponseFactoryInterface.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Electro\Interfaces\Http;
 
 use Psr\Http\Message\ResponseInterface;
@@ -9,25 +10,39 @@ interface ResponseFactoryInterface
 
   /**
    * Creates a new HTTP response object, compatible with ResponseInterface.
-   * @param int    $status
-   * @param string $content
-   * @param string $contentType
-   * @param array  $headers
+   *
+   * @param int    $status      HTTP status code.
+   * @param string $content     The message content, as a string.
+   * @param string $contentType If empty, no content type header will be set.
+   * @param array  $headers     HTTP response headers.
    * @return ResponseInterface
    */
-  function make ($status = 200, $content = '', $contentType = 'text/html', array $headers = []);
+  function make ($status = 200, $content = '', $contentType = '', array $headers = []);
 
   /**
    * Creates a new stream from the specified string, for use as an HTTP response body.
+   *
    * @param string          $content Initial body content.
    * @param string|resource $stream  Stream identifier and/or an actual stream resource.
    * @return StreamInterface
    */
-  function makeBody ($content = '', $stream = 'php://memory');
+  function makeBodyStream ($content = '', $stream = 'php://memory');
+
+  /**
+   * Creates a new HTTP response object, compatible with ResponseInterface.
+   *
+   * @param string|resource|StreamInterface $stream  Stream identifier and/or actual stream resource
+   * @param int                             $status  Status code for the response, if any.
+   * @param array                           $headers Headers for the response, if any.
+   * @throws \InvalidArgumentException on any invalid element.
+   * @return ResponseInterface
+   */
+  function makeFromStream ($stream = 'php://memory', $status = 200, array $headers = []);
 
   /**
    * Creates an HTML response with the given content.
    * <p>This is a convenience method.
+   *
    * @param string $content
    * @return ResponseInterface
    */
@@ -36,19 +51,10 @@ interface ResponseFactoryInterface
   /**
    * Creates a JSON response for the given data.
    * <p>This is a convenience method.
+   *
    * @param mixed $data
    * @return ResponseInterface
    */
   function makeJsonResponse ($data);
-
-  /**
-   * Creates a new HTTP response object, compatible with ResponseInterface.
-   * @param string|resource|StreamInterface $stream  Stream identifier and/or actual stream resource
-   * @param int                             $status  Status code for the response, if any.
-   * @param array                           $headers Headers for the response, if any.
-   * @throws \InvalidArgumentException on any invalid element.
-   * @return ResponseInterface
-   */
-  function makeStream ($stream = 'php://memory', $status = 200, array $headers = []);
 
 }
