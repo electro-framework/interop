439dbf4ab349a378b8fcf917d88ef121fbfea1cc
Cláudio Silva <claudio.silva@impactwave.com>
2017-03-17 19:46:53 +0000

    Doc.

--- Interfaces/Http/RedirectionInterface.php
+++ Interfaces/Http/RedirectionInterface.php
@@ -1,5 +1,6 @@
 <?php
 namespace Electro\Interfaces\Http;
+
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
 
@@ -14,10 +15,57 @@ interface RedirectionInterface
   /**
    * Creates a new redirection response to the previous location.
    * <p>If the previous location is not know, it redirects to the current URL.
+   *
    * @param int $status
    * @return \Psr\Http\Message\ResponseInterface
    */
   function back ($status = 302);
+
+  /**
+   * Creates a new redirection response, while saving the current URL in the session.
+   *
+   * @param string|UriInterface $url    A relative or an absolute URL. If empty, it is equivalent to the current URL.
+   * @param int                 $status HTTP status code.
+   * @return \Psr\Http\Message\ResponseInterface
+   */
+  function guest ($url, $status = 302);
+
+  /**
+   * Creates a new redirection response to the application's root URL.
+   *
+   * @param int $status
+   * @return \Psr\Http\Message\ResponseInterface
+   */
+  function home ($status = 302);
+
+  /**
+   * Creates a new redirection response to the previously intended location, which is the one set previously by calling
+   * `guest()`.
+   *
+   * @param string|UriInterface $defaultUrl A relative or an absolute URL to be used when a saved URL is not found on
+   *                                        the session. If empty, it is equivalent to the current URL.
+   * @param int                 $status     HTTP status code.
+   * @return \Psr\Http\Message\ResponseInterface
+   */
+  function intended ($defaultUrl = '', $status = 302);
+
+  /**
+   * Creates a new redirection response to the current URL.
+   *
+   * @param int $status
+   * @return \Psr\Http\Message\ResponseInterface
+   */
+  function refresh ($status = 302);
+
+  /**
+   * Creates a new redirection response to the given secure (https) URL.
+   *
+   * @param string|UriInterface $url    A relative or an absolute URL. If empty, it is equivalent to the root URL.
+   *                                    The protocol part is always replaced by 'https'.
+   * @param int                 $status HTTP status code.
+   * @return \Psr\Http\Message\ResponseInterface
+   */
+  function secure ($url, $status = 302);
 
   /**
    * Attaches a server request object to this instance, for use on subsequent generated redirection responses.
@@ -29,49 +77,14 @@ interface RedirectionInterface
   function setRequest (ServerRequestInterface $request);
 
   /**
-   * Creates a new redirection response, while saving the current URL in the session.
-   * @param string|UriInterface $url    A relative or an absolute URL. If empty, it is equivalent to the current URL.
-   * @param int                 $status HTTP status code.
-   * @return \Psr\Http\Message\ResponseInterface
-   */
-  function guest ($url, $status = 302);
-
-  /**
-   * Creates a new redirection response to the application's root URL.
-   * @param int $status
-   * @return \Psr\Http\Message\ResponseInterface
-   */
-  function home ($status = 302);
-
-  /**
-   * Creates a new redirection response to the previously intended location, which is the one set previously by calling
-   * `guest()`.
-   * @param string|UriInterface $defaultUrl A relative or an absolute URL to be used when a saved URL is not found on
-   *                                        the session. If empty, it is equivalent to the current URL.
-   * @param int                 $status     HTTP status code.
-   * @return \Psr\Http\Message\ResponseInterface
-   */
-  function intended ($defaultUrl = '', $status = 302);
-
-  /**
-   * Creates a new redirection response to the current URL.
-   * @param int $status
-   * @return \Psr\Http\Message\ResponseInterface
-   */
-  function refresh ($status = 302);
-
-  /**
-   * Creates a new redirection response to the given secure (https) URL.
-   * @param string|UriInterface $url    A relative or an absolute URL. If empty, it is equivalent to the current URL.
-   *                                    The protocol part is always replaced by 'https'.
-   * @param int                 $status HTTP status code.
-   * @return \Psr\Http\Message\ResponseInterface
-   */
-  function secure ($url, $status = 302);
-
-  /**
    * Creates a new redirection response to the given URL.
-   * @param string|UriInterface $url    A relative or an absolute URL. If empty, it is equivalent to the current URL.
+   *
+   * <p>If the URL begins with /, the redirection URL path will be set to that value exactly.
+   * <p>If the URL is relative, the redirection URL path will be set to the application's root path plus that URL.
+   * <p>If the URL is empty, the redirection will be performed to the application's root path.
+   * <p>If the URL is absolute, it will be used as is.
+   *
+   * @param string|UriInterface $url    A relative or an absolute URL.
    * @param int                 $status HTTP status code.
    * @return \Psr\Http\Message\ResponseInterface
    */
