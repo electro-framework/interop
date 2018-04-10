7f52db13f385fad8a8985452109e36161746b0f0
Cláudio Silva <claudio.silva@impactwave.com>
2018-04-10 17:03:11 +0100

    Doc.

--- Exceptions/FlashMessageException.php
+++ Exceptions/FlashMessageException.php
@@ -5,6 +5,13 @@ class FlashMessageException extends ExceptionWithTitle
 {
   protected $status;
 
+  /**
+   * FlashMessageException constructor.
+   *
+   * @param string $message Error message.
+   * @param int    $status  One of the {@see FlashType}::XXX constants.
+   * @param string $title
+   */
   public function __construct ($message, $status, $title = '')
   {
     parent::__construct ($title, $message, $status);
