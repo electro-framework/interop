2052a3416fe92b85ec11da8f4a8c763d74467ea7
Cláudio Silva <claudio.silva@impactwave.com>
2018-04-11 18:22:32 +0100

    Improved route matching interface, to avoid routing problems caused by the $modifiedRequest argument being a reference.

--- Interfaces/Http/RouteMatcherInterface.php
+++ Interfaces/Http/RouteMatcherInterface.php
@@ -1,5 +1,6 @@
 <?php
 namespace Electro\Interfaces\Http;
+
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
@@ -10,9 +11,9 @@ interface RouteMatcherInterface
   /**
    * Matches a request's URL against a route matching pattern.
    *
-   * <p>This method returns a new request object with a new path that results from the matched portion of the original
-   * one being consumed.
-   * > The new path may equals the original path if the pattern matches the initial location itself, and not a
+   * <p>Whan the match is successful, this method returns a new request object with a new URL path that results from the
+   * matched portion of the original URL path being consumed.
+   * > The new path may equal the original path if the pattern matches the initial location itself, and not a
    * sub-location.
    *
    * The new request object also provides all the route parameters defined on the pattern as request attributes with
@@ -22,10 +23,8 @@ interface RouteMatcherInterface
    *                                                details about the DSL syntax.
    * @param ServerRequestInterface $request         The HTTP request whose URL will be matched against the given
    *                                                pattern.
-   * @param ServerRequestInterface $modifiedRequest (output parameter) outputs the new request object, if changes to
-   *                                                the original are performed, otherwise it outputs the original
-   *                                                request.
-   * @return bool true if the pattern matches the path.
+   * @return ServerRequestInterface|bool false if the pattern doesn't match the path, a modified request instance
+   *                                                otherwhise.
    */
-  function match ($pattern, ServerRequestInterface $request, ServerRequestInterface &$modifiedRequest);
+  function match ($pattern, ServerRequestInterface $request);
 }
